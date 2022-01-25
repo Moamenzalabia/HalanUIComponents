0deabe57cc2117b24ec8e422a79a0b8332871692
Moamenzalabia <moamen.zalabia@vodafone.com>
2022-01-25 20:19:11 +0200

    Initial commit

---
+++ HalanUIComponents/HalanUIComponents.h
@@ -0,0 +1,18 @@
+//
+//  HalanUIComponents.h
+//  HalanUIComponents
+//
+//  Created by Moamen Abd Elgawad on 25/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HalanUIComponents.
+FOUNDATION_EXPORT double HalanUIComponentsVersionNumber;
+
+//! Project version string for HalanUIComponents.
+FOUNDATION_EXPORT const unsigned char HalanUIComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HalanUIComponents/PublicHeader.h>
+
+
